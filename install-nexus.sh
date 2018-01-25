80772a20f1b3a987cfa489dd20eb634e43927e51
udayakumarpalati <32889364+udayakumarpalati@users.noreply.github.com>
2018-01-25 11:42:15 +0530

    Update install-nexus.sh

--- install-nexus.sh
+++ install-nexus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Source Common Functions
-curl -s "https://raw.githubusercontent.com/udayakumarpalati/laborarecp/master/common-functions.sh" >/tmp/common-functions.sh
+#curl -s "https://raw.githubusercontent.com/udayakumarpalati/laborarecp/master/common-functions.sh" >/tmp/common-functions.sh
 #source /root/scripts/common-functions.sh
 source /tmp/common-functions.sh
 
@@ -19,41 +19,42 @@ DownloadJava 8
 
 ## Installing Java
 yum localinstall $JAVAFILE -y &>/dev/null
-if [ $? -eq 0 ]; then 
-	success "JAVA Installed Successfully"
+if [ $? -eq 0 ]; then
+        success "JAVA Installed Successfully"
 else
-	error "JAVA Installation Failure!"
-	exit 1
+        error "JAVA Installation Failure!"
+        exit 1
 fi
 
 ## Downloading Nexus
 yum install https://kojipkgs.fedoraproject.org/packages/python-html2text/2016.9.19/1.el7/noarch/python2-html2text-2016.9.19-1.el7.noarch.rpm -y &>/dev/null
-URL=$(curl -s https://help.sonatype.com/display/NXRM3/Download | html2text | grep unix.tar.gz | sed -e 's/>//g' -e 's/<//g' | grep ^http)
+URL=$(curl -s https://help.sonatype.com/display/NXRM3/Download | html2text | grep unix.tar.gz|grep Unix |sed -e 's/>//g' -e 's/<//g' -e 's/(//g' | cut -d "|" -f2)
+#URL=$(curl -s https://help.sonatype.com/display/NXRM3/Download | html2text | grep unix.tar.gz | sed -e 's/>//g' -e 's/<//g' | grep ^http)
 NEXUSFILE=$(echo $URL | awk -F '/' '{print $NF}')
 NEXUSDIR=$(echo $NEXUSFILE|sed -e 's/-unix.tar.gz//')
 NEXUSFILE="/opt/$NEXUSFILE"
 wget $URL -O $NEXUSFILE &>/dev/null
-if [ $? -eq 0  ]; then 
-	success "NEXUS Downloaded Successfully"
+if [ $? -eq 0  ]; then
+        success "NEXUS Downloaded Successfully"
 else
-	error "NEXUS Downloading Failure"
-	exit 1
+        error "NEXUS Downloading Failure"
+        exit 1
 fi
 
 ## Adding Nexus User
 id nexus &>/dev/null
-if [ $? -ne  0 ]; then 
-	useradd nexus
-	if [ $? -eq 0 ]; then 
-		success "Added NEXUS User Successfully"
-	else
-		error "Adding NEXUS User Failure"
-		exit 1
-	fi
+if [ $? -ne  0 ]; then
+        useradd nexus
+        if [ $? -eq 0 ]; then
+                success "Added NEXUS User Successfully"
+        else
+                error "Adding NEXUS User Failure"
+                exit 1
+        fi
 fi
 
 ## Extracting Nexus
-if [ ! -f "/home/nexus/$NEXUSDIR" ]; then 
+if [ ! -f "/home/nexus/$NEXUSDIR" ]; then
 su nexus <<EOF
 cd /home/nexus
 tar xf $NEXUSFILE
@@ -62,14 +63,14 @@ fi
 success "Extracted NEXUS Successfully"
 ## Setting Nexus starup
 unlink /etc/init.d/nexus &>/dev/null
-ln -s /home/nexus/$NEXUSDIR/bin/nexus /etc/init.d/nexus 
-echo "run_as_user=nexus" >/home/nexus/$NEXUSDIR/bin/nexus.rc
+ln -s /home/nexus/nexus-3.7.1-02/bin/nexus /etc/init.d/nexus
+echo "run_as_user=nexus" >/home/nexus/nexus-3.7.1-02/bin/nexus.rc
 success "Updating System Configuration"
 systemctl enable nexus &>/dev/null
 systemctl start nexus
-if [ $? -eq 0 ]; then 
-	success "Starting Nexus Service"
+if [ $? -eq 0 ]; then
+        success "Starting Nexus Service"
 else
-	error "Starting Nexus Failed"
-	exit 1
+        error "Starting Nexus Failed"
+        exit 1
 fi
